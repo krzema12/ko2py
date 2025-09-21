cb6814a50ed20fd02f8970b0a6f13cc7cbfa2907
Piotr Krzeminski <git@krzeminski.it>
2025-09-21 21:04:14 +0200

    Configure CI

---
+++ .github/workflows/build.main.kts
@@ -0,0 +1,29 @@
+#!/usr/bin/env kotlin
+
+@file:Repository("https://repo.maven.apache.org/maven2/")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:3.5.0")
+@file:Repository("https://bindings.krzeminski.it")
+@file:DependsOn("actions:checkout:v4")
+@file:DependsOn("gradle:actions__setup-gradle:v4")
+
+import io.github.typesafegithub.workflows.actions.actions.Checkout
+import io.github.typesafegithub.workflows.actions.gradle.ActionsSetupGradle
+import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
+import io.github.typesafegithub.workflows.domain.triggers.PullRequest
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.workflow
+
+workflow(
+    name = "Build",
+    on = listOf(
+        Push(branches = listOf("main")),
+        PullRequest(),
+    ),
+    sourceFile = __FILE__,
+) {
+    job(id = "build", runsOn = UbuntuLatest) {
+        uses(action = Checkout())
+        uses(action = ActionsSetupGradle())
+        run(command = "./gradlew build",)
+    }
+}
