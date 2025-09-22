7049f09f8b9f18ad05d78e001b636165adaa20af
Piotr Krzeminski <git@krzeminski.it>
2025-09-22 20:44:57 +0200

    Generate test cases from files

--- compiler-plugin/src/test/kotlin/it/krzeminski/ko2py/compiler/JvmAndPythonOutputTest.kt
+++ compiler-plugin/src/test/kotlin/it/krzeminski/ko2py/compiler/JvmAndPythonOutputTest.kt
@@ -3,27 +3,36 @@ package it.krzeminski.ko2py.compiler
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.engine.spec.tempfile
 import io.kotest.matchers.shouldBe
+import kotlin.io.path.Path
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.nameWithoutExtension
+import kotlin.io.path.walk
 
 class JvmAndPythonOutputTest : FunSpec({
-    // TODO: iterate over all Kotlin files in resources
-    test("smoke test") {
-        val input = KotlinToPythonTest::class.java.getResource("/HelloWorld.kt").readText()
+    Path("src/test/resources").walk()
+        .toList()
+        .filter { it.isRegularFile() && it.fileName.toString().endsWith(".kt") }
+        .also { require(it.isNotEmpty()) }
+        .forEach {
+            test(it.nameWithoutExtension) {
+                val input = KotlinToPythonTest::class.java.getResource("/" + it.fileName.toString()).readText()
 
-        val compilationResult = compileToPython(input)
-        val jvmClassFile = compilationResult.generatedFiles
-            .first { it.extension == "class" }
-            .parentFile
-        val pythonCodeString = compilationResult.getPythonOutput()
-        val pythonCode = tempfile()
-        pythonCode.writeText(pythonCodeString)
-        val stdoutFromJvm = runCommand("java", "-cp", jvmClassFile.absolutePath, "MainKt")
-        val stdoutFromPython = runCommand("python3", pythonCode.absolutePath)
+                val compilationResult = compileToPython(input)
+                val jvmClassFile = compilationResult.generatedFiles
+                    .first { it.extension == "class" }
+                    .parentFile
+                val pythonCodeString = compilationResult.getPythonOutput()
+                val pythonCode = tempfile()
+                pythonCode.writeText(pythonCodeString)
+                val stdoutFromJvm = runCommand("java", "-cp", jvmClassFile.absolutePath, "MainKt")
+                val stdoutFromPython = runCommand("python3", pythonCode.absolutePath)
 
-        val expectedStdout = input
-            .substringAfter("// Output:\n")
-            .lines().joinToString(separator = "\n") { it.removePrefix("//").trimStart() }
+                val expectedStdout = input
+                    .substringAfter("// Output:\n")
+                    .lines().joinToString(separator = "\n") { it.removePrefix("//").trimStart() }
 
-        stdoutFromPython shouldBe expectedStdout
-        stdoutFromJvm shouldBe expectedStdout
-    }
+                stdoutFromPython shouldBe expectedStdout
+                stdoutFromJvm shouldBe expectedStdout
+            }
+        }
 })
